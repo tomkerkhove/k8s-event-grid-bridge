0ad90d4cae6c8aab4b8809a06caf22ea18fa4cd4
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-03-21 12:14:00 -0700

    Use constant

--- src/Kubernetes.EventBridge.Host/CloudEvents/CloudEventsSchematizer.cs
+++ src/Kubernetes.EventBridge.Host/CloudEvents/CloudEventsSchematizer.cs
@@ -8,6 +8,7 @@ namespace Kubernetes.EventBridge.Host.CloudEvents
 {
     public class CloudEventsSchematizer
     {
+        private const string DefaultEventType = "Kubernetes.Event";
         /// <summary>
         ///     Generates a Cloud Event for a Kubernetes Event (v1 schema)
         /// </summary>
@@ -16,12 +17,11 @@ namespace Kubernetes.EventBridge.Host.CloudEvents
         {
             Guard.NotNull(kubernetesEvent, nameof(kubernetesEvent));
 
-            var eventType = "Kubernetes.Event";
             var eventSource = new Uri(uriString: "/subscriptions/0f9d7fea-99e8-4768-8672-06a28514f77e/resourceGroups/k8s-event-bridge/providers/Microsoft.EventGrid/topics/k8s-event-bridge#k8s-event-bridge", uriKind: UriKind.Relative);
             var eventId = kubernetesEvent.Metadata.Uid ?? Guid.NewGuid().ToString();
             var eventTime = kubernetesEvent.LastTimestamp ?? DateTime.UtcNow;
 
-            var cloudEvent = new CloudEvent(CloudEventsSpecVersion.V0_1, eventType, eventSource, eventId, eventTime)
+            var cloudEvent = new CloudEvent(CloudEventsSpecVersion.V0_1, DefaultEventType, eventSource, eventId, eventTime)
             {
                 ContentType = new ContentType(contentType: "application/json"),
                 Data = kubernetesEvent
