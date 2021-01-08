9ae114abb2a429fa88fc0f3d777ba6444185a4fb
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-01-08 07:51:17 +0100

    Rough basic version
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Kubernetes.EventBridge.Host/KubernetesEventExporterFunction.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading.Tasks;
+using Arcus.EventGrid.Publishing.Interfaces;
+using CloudNative.CloudEvents;
+using GuardNet;
+using k8s.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using ContentType = System.Net.Mime.ContentType;
+
+namespace Kubernetes.EventBridge.Host
+{
+    public class KubernetesEventExporterFunction
+    {
+        private readonly ILogger<KubernetesEventExporterFunction> _logger;
+        private readonly IEventGridPublisher _eventGridPublisher;
+
+        public KubernetesEventExporterFunction(IEventGridPublisher eventGridPublisher, ILogger<KubernetesEventExporterFunction> logger)
+        {
+            Guard.NotNull(eventGridPublisher, nameof(eventGridPublisher));
+            Guard.NotNull(logger, nameof(logger));
+
+            _logger = logger;
+            _eventGridPublisher = eventGridPublisher;
+        }
+
+        [FunctionName("kubernetes-event-exporter")]
+        public async Task Run([HttpTrigger(AuthorizationLevel.Anonymous, Route = "kubernetes/events/forward")] HttpRequest request)
+        {
+            var payload = await request.ReadAsStringAsync();
+            _logger.LogInformation($"Kubernetes event received: {payload}");
+
+            var cloudEvent = ConvertToCloudEvent(payload);
+            await _eventGridPublisher.PublishAsync(cloudEvent);
+
+            ILoggerExtensions.LogMetric(_logger, "Kubernetes Event Published", 1);
+        }
+
+        private static CloudEvent ConvertToCloudEvent(string payload)
+        {
+            var @event = JsonConvert.DeserializeObject<Eventsv1Event>(payload);
+            var @event2 = JsonConvert.DeserializeObject<Corev1Event>(payload);
+            var @event3 = JsonConvert.DeserializeObject<V1beta1Event>(payload);
+
+            var cloudEvent = new CloudEvent(CloudEventsSpecVersion.V1_0, "Kubernetes.Events.Generic", new Uri("http://temporary"))
+            {
+                DataContentType = new ContentType("application/json"), 
+                Data = @event2
+            };
+
+            return cloudEvent;
+        }
+    }
+}
---
+++ src/Kubernetes.EventBridge.Host/Startup.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Arcus.EventGrid.Publishing;
+using Arcus.EventGrid.Publishing.Interfaces;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Events;
+
+[assembly: FunctionsStartup(typeof(Kubernetes.EventBridge.Host.Startup))]
+namespace Kubernetes.EventBridge.Host
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            base.ConfigureAppConfiguration(builder);
+
+            builder.ConfigurationBuilder.AddEnvironmentVariables("EventBridge_");
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var configuration = GetConfiguration(builder);
+
+            AddEventGridPublisher(builder, configuration);
+            ConfigureLogging(builder, configuration);
+        }
+
+        private static void AddEventGridPublisher(IFunctionsHostBuilder builder, IConfiguration configuration)
+        {
+            var topicEndpoint = configuration["EventGrid_Topic_Uri"];
+            var topicKey = configuration["EventGrid_Topic_Key"];
+
+            var eventGridPublisher = EventGridPublisherBuilder
+                .ForTopic(topicEndpoint)
+                .UsingAuthenticationKey(topicKey)
+                .Build();
+
+            builder.Services.AddTransient<IEventGridPublisher>(provider => eventGridPublisher);
+        }
+
+        private static void ConfigureLogging(IFunctionsHostBuilder builder, IConfiguration configuration)
+        {
+            var instrumentationKey = configuration.GetValue<string>("APPINSIGHTS_INSTRUMENTATIONKEY");
+
+            var loggerConfiguration = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .Enrich.WithComponentName("Kubernetes Event Bridge")
+                .Enrich.WithVersion()
+                .WriteTo.Console();
+
+            if (string.IsNullOrWhiteSpace(instrumentationKey) == false)
+            {
+                loggerConfiguration = loggerConfiguration.WriteTo.AzureApplicationInsights(instrumentationKey);
+            }
+                
+            var logger = loggerConfiguration.CreateLogger();
+
+            builder.Services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.ClearProvidersExceptFunctionProviders();
+                loggingBuilder.AddSerilog(logger);
+            });
+        }
+
+        private static IConfiguration GetConfiguration(IFunctionsHostBuilder builder)
+        {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            return configuration;
+        }
+    }
+}
