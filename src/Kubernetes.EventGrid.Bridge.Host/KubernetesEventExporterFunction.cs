5311b35e8f13341bf55141594295912b33ab0d3f
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-04-14 20:51:18 +0200

    Provide X-Event-Id header for emitted events
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Kubernetes.EventGrid.Bridge.Host/KubernetesEventExporterFunction.cs
+++ src/Kubernetes.EventGrid.Bridge.Host/KubernetesEventExporterFunction.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Arcus.EventGrid.Publishing.Interfaces;
+using CloudNative.CloudEvents;
 using GuardNet;
 using Kubernetes.EventGrid.Core.CloudEvents.Interfaces;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -26,14 +29,34 @@ namespace Kubernetes.EventGrid.Bridge.Host
         }
 
         [FunctionName("kubernetes-event-exporter")]
-        public async Task Run([HttpTrigger(AuthorizationLevel.Anonymous, Route = "kubernetes/events/forward")] HttpRequest request)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, Route = "kubernetes/events/forward")] HttpRequest request)
+        {
+            var cloudEvent = await ConvertRequestToCloudEvent(request);
+            
+            await PublishEventAsync(cloudEvent);
+            
+            MeasureEventPublished();
+            
+            request.HttpContext.Response.Headers.TryAdd("X-Event-Id", cloudEvent.Id);
+            return new OkResult();
+        }
+
+        private async Task<CloudEvent> ConvertRequestToCloudEvent(HttpRequest request)
         {
             var payload = await request.ReadAsStringAsync();
             _logger.LogInformation($"Kubernetes event received: {payload}");
 
             var cloudEvent = _cloudEventFactory.CreateFromRawKubernetesEvent(payload);
-            await _eventGridPublisher.PublishAsync(cloudEvent);
+            return cloudEvent;
+        }
 
+        private async Task PublishEventAsync(CloudEvent cloudEvent)
+        {
+            await _eventGridPublisher.PublishAsync(cloudEvent);
+        }
+
+        private void MeasureEventPublished()
+        {
             ILoggerExtensions.LogMetric(_logger, "Kubernetes Event Published", 1);
         }
     }
